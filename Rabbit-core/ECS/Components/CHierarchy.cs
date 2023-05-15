6d138251a34e3a391c0a39205c4b15ee06c07940
tkzc00 <2238983030@qq.com>
2023-05-15 19:47:57 +0800

    Hierarchy层级组件定义完成

---
+++ Rabbit-core/ECS/Components/CHierarchy.cs
@@ -0,0 +1,15 @@
+﻿namespace Rabbit_core.ECS.Components;
+
+public class CHierarchy : IComponent
+{
+    public Guid Id { get; }
+    public Guid? ParentId { get; internal set; }
+    public List<Guid> Children { get;}
+
+    public CHierarchy(Guid id)
+    {
+        Id = id;
+        ParentId = null;
+        Children = new List<Guid>();
+    }
+}
