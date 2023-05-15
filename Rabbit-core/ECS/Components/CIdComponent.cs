a51f7ebe1894f5b3016fbcabb5f9f78967c4c07a
tkzc00 <2238983030@qq.com>
2023-05-15 19:08:06 +0800

    Id组件

---
+++ Rabbit-core/ECS/Components/CIdComponent.cs
@@ -0,0 +1,11 @@
+﻿namespace Rabbit_core.ECS.Components;
+
+public class CIdComponent : IComponent
+{
+    public Guid Id { get; }
+
+    public CIdComponent(Guid id)
+    {
+        Id = Guid.NewGuid();
+    }
+}
